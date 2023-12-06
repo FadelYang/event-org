16fdc5db1bcc8679bf750b432d079d236eeae2bb
fadelanumah <fadelanumah@gmail.com>
2023-12-07 04:38:14 +0700

    feat(homepage): Add Image Clickable

--- resources/views/pages/home/main-hero.blade.php
+++ resources/views/pages/home/main-hero.blade.php
@@ -4,10 +4,10 @@
          <h1 class="text-2xl md:text-3xl lg:text-5xl font-bold">Temukan Event Menarik Dan Bermanfat, Atau Buat Dan
              Promosikan Eventmu Sendiri</h1>
          <div class="block md:flex gap-3 mt-5 items-start justify-center lg:justify-start">
-             <button class="py-2 px-5 border-2 border-black rounded text-lg md:text-xl text-gray-200">Buat Event</button>
-             <button
+             <a class="py-2 px-5 border-2 border-black rounded text-lg md:text-xl text-gray-200">Buat Event</a>
+             <a href="#eventPilihan"
                  class="mt-3 md:mt-0 py-2 px-5 border-2 border-black rounded text-lg md:text-xl text-gray-200 bg-gray-800">Telusuri
-                 Event</button>
+                 Event</a>
          </div>
      </div>
      <div class="my-10 hidden lg:block">
