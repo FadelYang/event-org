44e30e758d3d709e00f521a6243125e37d3d39b2
fadelanumah <fadelanumah@gmail.com>
2023-12-07 00:14:25 +0700

    feat(homepage): Add Event Pilihan Menu

---
+++ resources/views/pages/home/event-pilihan.blade.php
@@ -0,0 +1,30 @@
+<div class="section-header my-10 block md:flex items-center">
+    <p class="text-2xl font-extrabold md:text-4xl me-auto">Event Pilihan</p>
+</div>
+
+<div class="">
+    <div class="section-content flex flex-nowrap gap-3 overflow-x-scroll snap-x snap-mandatory no-scrollbar">
+        @for ($i = 0; $i < 10; $i++)
+            <div class="event-card bg-gray-200 max-w-sm md:max-w-xs rounded-xl min-w-max snap-center">
+                <img src="{{ fake()->imageUrl($width = 328, $height = 220) }}" alt="" class="w-full rounded-t-xl">
+                <div class="event-card-detail mx-2">
+                    <div class="flex mt-1 between">
+                        <p class="text-xl font-bold me-auto">Nama Event</p>
+                        <p class="text-lg font-bold">Online</p>
+                    </div>
+                    <div class="mt-1">
+                        <p class="text-lg text-gray-500">{{ fake()->date('d M Y') }}</p>
+                        <p class="mt-3 font-bold text-lg">Gratis</p>
+                    </div>
+                    <div class="event-organizer flex items-center gap-3 mt-1 py-2 border-t-2 border-gray-500">
+                        <img src="{{ fake()->imageUrl($width = 35, $height = 35) }}" alt=""
+                            class="rounded-full">
+                        <p class="text-lg">{{ fake()->name() }}</p>
+                    </div>
+                </div>
+
+            </div>
+        @endfor
+
+    </div>
+</div>
--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -1,17 +1,6 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        {{-- hero banner --}}
-        <div class="flex gap-8 items-center justify-center px-5 py-10 lg:p-5 hero-banner rounded">
-            <div class="max-w-full text-center lg:max-w-xl lg:text-start">
-                <h1 class="text-2xl md:text-3xl lg:text-5xl font-bold">Temukan Event Menarik Dan Bermanfat, Atau Buat Dan Promosikan Eventmu Sendiri</h1>
-                <div class="block md:flex gap-3 mt-5 items-start justify-center lg:justify-start">
-                    <button class="py-2 px-5 border-2 border-black rounded text-lg md:text-xl text-gray-200">Buat Event</button>
-                    <button class="mt-3 md:mt-0 py-2 px-5 border-2 border-black rounded text-lg md:text-xl text-gray-200 bg-gray-800">Telusuri Event</button>
-                </div>
-            </div>
-            <div class="my-10 hidden lg:block">
-                <img src="{{ fake()->imageUrl($width=400, $height = 400) }}" alt="" class="rounded-lg">
-            </div>
-        </div>
+        @include('pages.home.main-hero')
+        @include('pages.home.event-pilihan')
     </div>
 </x-app-layout>
