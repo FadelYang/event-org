f7c2539fe5fcf4e93aa0b1ded17220bf9b9d5ed9
fadelanumah <fadelanumah@gmail.com>
2023-12-07 08:09:26 +0700

    feat(homepage): Add Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,37 @@
+<footer class="mt-10 bg-gray-800 text-white">
+    <div class="max-w-7xl mx-auto pt-20 pb-32 px-4 sm:px-6 lg:px-8 flex flex-wrap gap-10">
+        <div>
+            <h1 class="font-bold mb-5">Tentang Evenin</h1>
+            <ul class="">
+                <li><a href="/#" class="opacity-80 hover:opacity-100">Tentang Kami</a></li>
+                <li><a href="/#" class="opacity-80 hover:opacity-100">Bergabung Bersama Kami</a></li>
+                <li><a href="/#" class="opacity-80 hover:opacity-100">Syarat Dan Ketentuan</a></li>
+            </ul>
+        </div>
+        <div>
+            <h1 class="font-bold mb-5">Daftarkan Event</h1>
+            <ul class="">
+                <li><a href="/#" class="opacity-80 hover:opacity-100">Daftarkan Event</a></li>
+                <li><a href="/#" class="opacity-80 hover:opacity-100">Promosikan Event</a></li>
+            </ul>
+        </div>
+        <div>
+            <h1 class="font-bold mb-5">Cari Event</h1>
+            <ul class="">
+                <li><a href="/#" class="opacity-80 hover:opacity-100">Daftar Event Yang Akan Berlangsung</a></li>
+                <li><a href="/#" class="opacity-80 hover:opacity-100">Event Pelatihan</a></li>
+                <li><a href="/#" class="opacity-80 hover:opacity-100">Event Lomba</a></li>
+            </ul>
+        </div>
+        <div>
+            <h1 class="font-bold mb-5">Hubungi Kami</h1>
+            <ul class="flex gap-3">
+                <li><a href="/#"><img class="opacity-80 hover:opacity-100" src="{{ fake()->imageUrl($width=35, $height=35) }}" alt=""></a></li>
+                <li><a href="/#"><img class="opacity-80 hover:opacity-100" src="{{ fake()->imageUrl($width=35, $height=35) }}" alt=""></a></li>
+                <li><a href="/#"><img class="opacity-80 hover:opacity-100" src="{{ fake()->imageUrl($width=35, $height=35) }}" alt=""></a></li>
+                <li><a href="/#"><img class="opacity-80 hover:opacity-100" src="{{ fake()->imageUrl($width=35, $height=35) }}" alt=""></a></li>
+                <li><a href="/#"><img class="opacity-80 hover:opacity-100" src="{{ fake()->imageUrl($width=35, $height=35) }}" alt=""></a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
