53089716d123a22dd903de89981c1ced26d77c7a
fadelanumah <fadelanumah@gmail.com>
2023-12-11 18:24:59 +0700

    feat: Add Bredcrumb For Home And Event

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,22 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+// home
+Breadcrumbs::for('home', function (BreadcrumbTrail $trail) {
+    $trail->push('Home', route('home'));
+});
+
+// home > event
+Breadcrumbs::for('event', function (BreadcrumbTrail $trail) {
+    $trail->parent('home');
+    $trail->push('event', route('event.get'));
+});
+
+// home > event > event_type > event_name
+Breadcrumbs::for('detailEvent', function (BreadcrumbTrail $trail, $event) {
+   $trail->parent('event');
+   $trail->push($event[0], route('event.detail', [$event[0], $event[1]])); 
+   $trail->push($event[1], route('event.detail', [$event[0], $event[1]])); 
+});
