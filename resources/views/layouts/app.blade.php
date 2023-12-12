48b0308a310f4bf4c4f1b133c75d96430e82828a
fadelanumah <fadelanumah@gmail.com>
2023-12-12 07:14:47 +0700

    feat(detail-event): Change Header Background Colour

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 
         <!-- Page Heading -->
         @if (isset($header))
-            <header class="bg-gray-800 shadow">
+            <header class="hero-banner shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
