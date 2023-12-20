0cc62fe77627258cc742f17dfc8a5ee9a89842cf
fadelanumah <fadelanumah@gmail.com>
2023-12-20 20:03:37 +0700

    feat(create-event): Add Breadcrumb And Footer

--- resources/views/pages/event/create/create-event.blade.php
+++ resources/views/pages/event/create/create-event.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <p>Halo</p>
-</body>
-</html>
+<x-app-layout>
+    @include('pages.event.create.header')
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        
+    </div>
+    @include('components.footer')
+</x-app-layout>
