a20c487d8f14a8aa81439adc1043d2e1f157cb73
fadelanumah <fadelanumah@gmail.com>
2023-12-28 10:12:14 +0700

    Fix(detail-event): Fix Icon Too Small If Text Too Long

--- resources/views/pages/event/detail/event-detail-header.blade.php
+++ resources/views/pages/event/detail/event-detail-header.blade.php
@@ -15,7 +15,7 @@
         <p class="hidden lg:block"> - </p>
         <div class="flex items-center gap-1">
             <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-5 w-5">
+                xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-5 w-5 min-h-[1.25rem] min-w-[1.25rem]">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path>
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"></path>
