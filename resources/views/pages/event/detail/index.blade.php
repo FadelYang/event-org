72b840209fe5e3b34e90b26fc380c91642c7335d
fadelanumah <fadelanumah@gmail.com>
2023-12-30 12:43:34 +0700

    fix(event-detail): Fix Content Layout

--- resources/views/pages/event/detail/index.blade.php
+++ resources/views/pages/event/detail/index.blade.php
@@ -1,16 +1,14 @@
 <x-app-layout>
     @include('pages.event.detail.event-detail-header')
 
-    <div class="mt-3 max-w-7xl mx-1 lg:px-8 xl:mx-auto h-auto">
+    <div class="mt-3 max-w-7xl mx-1 lg:px-8 xl:mx-auto">
         @include('pages.event.detail.event-detail-first-row')
         @include('pages.event.detail.event-detail-second-row')
         @if (count($eventTickets) > 0)
             @include('pages.event.detail.ticket-section')
         @else
-           <p class="text-md lg:text-lg mt-5">Belum ada info penjualan tiket</p>
+            <p class="text-md lg:text-lg mt-5">Belum ada info penjualan tiket</p>
         @endif
     </div>
-    <div class="">
-        @include('components.footer')
-    </div>
+    @include('components.footer')
 </x-app-layout>
